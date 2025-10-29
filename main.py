1767af055bee7baa4c72fe38e9b02e323093a0e7
RyuuMasato <jorrit.meulenbeld@outlook.com>
2025-10-29 17:00:19 +0100

    updated main to use groups and container for updatable and drawable sprite game objects

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    # After changing a static field like containers, make sure to create all Player objects after the change. This way, they will be correctly added to the groups.
+    Player.containers = (updatable, drawable)
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while True:
@@ -21,15 +27,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        # update player
-        player.update(dt)
+        # update group (player and others in group)
+        for sprite in updatable:
+            sprite.update(dt)
 
         # draw black background
         black = pygame.Color(0, 0, 0, a=255)
         screen.fill(black)
 
-        # draw player
-        player.draw(screen)
+        # draw group (player and others in group)
+        for sprite in drawable:
+            sprite.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
